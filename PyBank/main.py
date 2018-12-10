2dc10f28b7c923d45415f416335e3b69fbb88d90
Natalia <natalia1andriychuk@gmail.com>
2018-12-09 19:23:27 -0500

    total net amount of Profit/Losses over the entire period found

--- PyBank/main.py
+++ PyBank/main.py
@@ -30,20 +30,39 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 # Open the CSV
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+    #print(csvreader)
 
-    #counting number of rows in the dataset (minus the header)
-    #as it represents the number of months in this case
+    # counting number of rows in the dataset (minus the header)
+    # as it represents the number of months in this case
     data = list(csvreader)
     months_count = len(data) - 1
+    
 # print("Financial Analysis:")
 # print("------------------------------")
 # print(f'Total Months: {months_count}')
 
-    # total = 0.0
-    # for row in data:
-        #total  = row[1]
-column = 1
-print (sum(row[column] for row in data))
+#column_sum = []
+#Iterating thoroug the "Profit/Losses" column to sum all the values
+total = 0
+for row in data:
+    #column_sum.append(row[1])
+
+    #for number in column_sum: ----I wa trying to do the nested for loop here since I thought 
+    #that I need to iterate thtough eact munber in the column sum, since I thought that 
+    # we ned to put it one list and then iterate through it, but I was able to iterate throug the row
+    total = total + int(row[1])
+#print(f'Total: ${total}')
+
+    print(row[0])
+
+#type(row))
+# column_sum  = [row.sum() for row in data]
+# print (column_sum)
+        #print(row[1])
+# column = 1
+# print (sum(row[column] for row in data))
 
 # rows = csvreader.DictReader(csvfile)
 # print (sum(float(csvfile['Profit/Losses']) for csvfile in rows) 
