aa38461bd70709a7edd1779faada19d6c0e3fccc
Natalia <natalia1andriychuk@gmail.com>
2018-12-10 23:21:30 -0500

    semi-final

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,4 @@
 # Your task is to create a Python script that analyzes the records to calculate each of the following:
-
-
 # The total number of months included in the dataset
 # The total net amount of "Profit/Losses" over the entire period
 # The average change in "Profit/Losses" between months over the entire period
@@ -37,45 +35,75 @@ with open(csvpath, newline="") as csvfile:
     # counting number of rows in the dataset (minus the header)
     # as it represents the number of months in this case
     data = list(csvreader)
-    months_count = len(data) - 1
+    months_count = len(data)
     
-# print("Financial Analysis:")
-# print("------------------------------")
-# print(f'Total Months: {months_count}')
+print("Financial Analysis:")
+print("--------------------------------")
+print(f'Total Months: {months_count}')
 
 profit_loss_column = []
+months_column = []
 #fail - ask why
 # profit_loss_column = [profit_loss_column for row in data]
 # print(profit_loss_column)
-#Iterating thoroug the "Profit/Losses" column to sum all the values
+# Iterating thoroug the "Profit/Losses" column to sum all the values
 total = 0
 for row in data:
     profit_loss_column.append(row[1])
-
+    months_column.append(row[0])
 
     #for number in column_sum: ----I wa trying to do the nested for loop here since I thought 
     #that I need to iterate thtough eact munber in the column sum, since I thought that 
     # we ned to put it one list and then iterate through it, but I was able to iterate throug the row
     total = total + int(row[1])
-#print(f'Total: ${total}')
+print(f'Total: ${total}')
+# print(months_column)
 
 def average_change():
     current_index = 0
-    next_index = 1
-    average_change = []
-    for change_value in profit_loss_column:
-        change = change_value[next_index] - change_value[current_index]
-        average_change.append(change_value)
-        current_index  =+ 1
-        next_index =+ 1
-    return average_change
+    next_index = current_index + 1
+    months_change_list = []
+
+    # for value in profit_loss_column:
+    for value in range(len(profit_loss_column)-1):
+        change = int(profit_loss_column[next_index]) - int(profit_loss_column[current_index])
+        months_change_list.append(change)
+        current_index += 1
+        next_index = current_index + 1
+
+        # calculating the average change by summing all values in the created 
+        # months_change_list and dividing them by total months - 1 since the first change
+        # occurs after the second month 
+        average_change = round(sum(months_change_list)/(months_count - 1),2)
+
+        #finding max and min values on the months_change_list
+        max_value = max(months_change_list)
+        min_value = min(months_change_list)
+
+        # Looking up the max and min value index in order to find out the corresponding
+        # month value in the month_column.  
+
+        index_max_value = months_change_list.index(max_value)
+        index_min_value = months_change_list.index(min_value)
+        increase_month = months_column[index_max_value + 1]
+        decrease_month = months_column[index_min_value + 1] 
+    return print(f'Average Change: {average_change}\nGreatest Increase in Profits: {increase_month} (${max_value})\nGreatest Decrease in Profits: {decrease_month} (${min_value}) ')
+average_change()
+
+#print(f'Total Months: {months_count}')
+
+#Average  Change: $-2315.12
+#   Greatest Increase in Profits: Feb-2012 ($1926159)
+#   Greatest Decrease in Profits: Sep-2013 ($-2196167)
+
+
+#for value in average_change:
+#you'll need that for task 4 and 5
+    # max_value = max(average_change)
+    # min_value = min(average_change)
+    #print(max_value, min_value)
+#print(min_value)
     
-
-
-
-
-
-
 # for x in profit_loss_column:
 #     profit_change = [x[i] - x[i+1] for x in profit_loss_column]
 # print(profit_change)
@@ -86,10 +114,7 @@ def average_change():
 
 
     #print(row[0])
-# you'll need that for task 4 and 5
-# max_value = max(profit_loss_column)
-# min_value = min(profit_loss_column)
-# print(max_value, min_value)
+#
 
 #type(row))
 # column_sum  = [row.sum() for row in data]
